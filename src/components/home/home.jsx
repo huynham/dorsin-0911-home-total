70015acff830f66204ee90a89bfcca6eb19842da
huynham <91304084+huynham@users.noreply.github.com>
2021-11-14 19:52:51 +0700

    first commit

---
+++ src/components/home/home.jsx
@@ -0,0 +1,44 @@
+
+
+import './home.css'
+
+function Home(){
+
+    return(
+        <section id="home">
+            <div className="home">
+                <div className="d1">
+                    <a href="https://huynham.github.io/dorsin-0911-home/" target="_blank" >Dorsin Home one <br/> Click Me !!!</a>
+
+                </div>
+                <div className="d2">
+                    <a href="https://huynham.github.io/dorsin-0911-home-2/" target="_blank" >Dorsin Home two <br/> Click Me !!!</a>
+                </div>
+                <div className="d3">
+                    <a href="https://huynham.github.io/dorsin-0911-home-3/" target="_blank" >Dorsin Home three <br/> Click Me !!!</a>
+                </div>
+                <div className="d4">
+                    <a href="https://huynham.github.io/dorsin-0911-home-four/" target="_blank" >Dorsin Home four <br/> Click Me !!!</a>
+                </div>
+                <div className="d5">
+                    <a href="https://huynham.github.io/dorsin-0911-home-five/" target="_blank" >Dorsin Home five <br/> Click Me !!!</a>
+                </div>
+                <div className="d6">
+                    <a href="https://huynham.github.io/dorsin-0911-home-six/" target="_blank" >Dorsin Home six <br/> Click Me !!!</a>
+                </div>
+                <div className="d7">
+                    <a href="https://huynham.github.io/dorsin-0911-home-seven/" target="_blank" >Dorsin Home seven <br/> Click Me !!!</a>
+                </div>
+                <div className="d8">
+                    <a href="https://huynham.github.io/dorsin-0911-home-eight/" target="_blank" >Dorsin Home eight <br/> Click Me !!!</a>
+                </div>
+                <div className="d9">
+                    <a href="https://huynham.github.io/dorsin-0911-home-nine/" target="_blank" >Dorsin Home nine <br/> Click Me !!!</a>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Home
